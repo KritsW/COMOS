9908ad0dff8366dd3e09fd8a2120fd33086fc97f
Kritsanapon <fifa15156@hotmail.com>
2023-10-11 16:27:39 +0700

    first commit

--- snap/firefox/common/.mozilla/firefox/g5a55k5d.default/prefs.js
+++ snap/firefox/common/.mozilla/firefox/g5a55k5d.default/prefs.js
@@ -48,7 +48,7 @@ user_pref("browser.startup.couldRestoreSession.count", 2);
 user_pref("browser.startup.homepage_override.buildID", "20231009233052");
 user_pref("browser.startup.homepage_override.mstone", "118.0.2");
 user_pref("browser.startup.lastColdStartupCheck", 1697015008);
-user_pref("browser.topsites.contile.lastFetch", 1697015309);
+user_pref("browser.topsites.contile.lastFetch", 1697016211);
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":19,\"newElementCount\":2}");
 user_pref("browser.urlbar.placeholderName", "Google");
 user_pref("browser.urlbar.quicksuggest.migrationVersion", 2);
